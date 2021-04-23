96eb95bdb9c7a54705ede7d010a365d5d3725d84
Neto <nivaldogomes11@hotmail.com>
2021-04-22 23:36:40 -0300

    Algumas questões

--- Mundo 2/ex_049.py
+++ Mundo 2/ex_049.py
@@ -2,3 +2,6 @@
 Refaça o DESAFIO 009, mostrando a tabuada de um número
 que o usuário escolher, só que agora utilizando um laço for.
 """
+print('=-=' * 15)
+print('                   TABUADA')
+print('=-=' * 15)
---
+++ Python_Aulas/aula_12.py
@@ -0,0 +1,15 @@
+"""
+For in Python
+Iterando Strings com  for
+Função range(start=0, stop, step)
+"""
+texto = 'Python'
+
+for letra in texto:
+    print(letra)
+
+for n in range(0, 100, 8):  # Acha os múltiplos de 8
+    print(n)
+
+# continue - pula para  o próximo laço.
+# brake - termina o laço.
